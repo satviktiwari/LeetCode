1dfe4a0cffc22e7c5083b618a684cdd701fe5453
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2022-06-28 19:08:43 +0530

    Time: 322 ms (11.08%), Space: 58.1 MB (7.43%) - LeetHub

--- 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.cpp
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxConsecutive(int bottom, int top, vector<int>& special) {
-        priority_queue<int, vector<int>, greater<int>>pq;
-        for(int i = 0; i < special.size(); i++){
-            pq.push(special[i]);
-        }
-        for(int i = 0; i < special.size(); i++){
-            special[i] = pq.top();
-            pq.pop();
-        }
+        // priority_queue<int, vector<int>, greater<int>>pq;
+        // for(int i = 0; i < special.size(); i++){
+        //     pq.push(special[i]);
+        // }
+        // for(int i = 0; i < special.size(); i++){
+        //     special[i] = pq.top();
+        //     pq.pop();
+        // }
+        sort(special.begin(), special.end());
         int ans = special[0]-bottom;
         for(int i = 1; i < special.size(); i++){
             ans = max(ans, (special[i]-special[i-1]-1));
