65fa876bd0213603f891128ddd4cf5768d6c9816
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2022-07-04 08:52:34 +0530

    Time: 2 ms (64.34%), Space: 5.9 MB (41.60%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        vector<int>v(31, 0);
+        v[0] = 0;
+        v[1] = 1;
+        for(int i = 2; i < 31; i++){
+            v[i] = v[i-1]+v[i-2];
+        }
+        return v[n];
+    }
+};
