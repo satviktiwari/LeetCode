22bdca3e4651d26bf3725e0f783cb6bfcebf20a8
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2022-07-08 09:27:22 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.17%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>st1, st2;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '#'){
+                if(st1.size()){
+                    st1.pop();
+                }
+            }
+            else{
+                st1.push(s[i]);
+            }
+        }
+        for(int i = 0; i < t.size(); i++){
+            if(t[i] == '#'){
+                if(st2.size()){
+                    st2.pop();
+                }
+            }
+            else{
+                st2.push(t[i]);
+            }
+        }
+        return st1 == st2;
+    }
+};
